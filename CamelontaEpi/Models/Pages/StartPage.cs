01cc3fcf87cfd09857e5faaf06156b949f135d47
Helena Hedström <31826611+helenahedstrom@users.noreply.github.com>
2019-01-09 09:36:04 +0100

    Added some helpers and startpage controller/view

---
+++ CamelontaEpi/Models/Pages/StartPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace CamelontaEpi.Models.Pages
+{
+    [ContentType(DisplayName = "Start", GUID = "19b3908c-a881-4062-8daa-357c9be3726c", 
+        Description = "The home page",
+        GroupName = SiteGroupNames.Specialized,
+        Order = 10)]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Heading", Description =
+        "If the Heading is not set, the page falls back to showing the Name.",
+        GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Main body",
+        Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+        GroupName = SystemTabNames.Content, Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
---
+++ CamelontaEpi/SiteGroupNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CamelontaEpi
+{
+    public class SiteGroupNames
+    {
+        public const string Specialized = "Specialized";
+        public const string Common = "Common";
+        public const string Blog = "Blog";
+    }
+}
